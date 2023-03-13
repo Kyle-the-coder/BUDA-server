3b0d3e728a3b95fa2be5246460c89440a07f5af4
Kyle Mitchell <kylethacoder@gmail.com>
2023-03-13 12:00:21 -0700

    updated

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config({path:'../.env'})
+const dotenv = require('dotenv').config({ path: '../.env' })
 
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+            .then(() => console.log('Db connected'))
+            .catch((err) => console.log('an error', err))
+    } catch (error) {
+        console.log(error)
+    }
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Db connected', process.env.MONGO_URI))
-    .catch((err) => console.log('an error', err))
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config({path:'../.env'})
-
+const connectDB = require('./config/mongoose.config')
 const cors = require('cors');
 const port = 8000 || process.env.PORT;
 const app = express();
@@ -9,4 +9,14 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended: true}));
 require("./config/mongoose.config");
 require("./routes/buda.routes")(app);
-app.listen(port, ()=> {console.log('you are now on port ' + port)});
+
+const startServer = async () =>{
+    try {
+        await connectDB()
+        app.listen(port, ()=> {console.log('you are now on port ' + port)});
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
